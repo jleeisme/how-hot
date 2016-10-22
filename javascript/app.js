8dbaa4bde0a12f0abf0ff79a9c029036e7dce7d2
Jason Lee <jleeisme@gmail.com>
2016-10-21 18:07:47 -0700

    Changes color on both inputs
    Added another code to use for a potential refactor

--- javascript/app.js
+++ javascript/app.js
@@ -21,7 +21,7 @@ function getRandomColor() {
   return color;
 }
 
-$('#temp-celsius').on('blur keyup', function() {
+$('#temp-celsius, #temp-fahrenheit').on('blur keyup', function() {
   $('body').css('background-color', getRandomColor());
 });
 
@@ -54,3 +54,63 @@ fahrenheit.onkeyup = function() {
 //  }
 // }
 
+// $(function() {
+//     $("#start").on('keyup', function() {
+//       $("#finish").val(celsiusToFarenheit($(this).val()));
+//     });
+//     $("#finish").on('keyup', function() {
+//       $("#start").val(fahrenheitToCelsius($(this).val()));
+//     });
+//   });
+
+// document.getElementById('celsius').onkeyup = updateFahrenheit;
+
+// document.getElementById('fahrenheit').onkeyup = 
+//   updateCelsius;
+
+// document.getElementById('celsius').value = 0;
+
+// // initial value on page load
+// updateFahrenheit();
+
+// function updateFahrenheit() {
+//   var c = document.getElementById('celsius').value;
+//   var f;
+//   if ( (c == null) || (c == "") || (isNaN(c) ) )
+//   {
+//     f = "";
+//   }
+//   else
+//   {
+//     f = celsiusToFahrenheit(c);
+//   }
+//   document.getElementById('fahrenheit').value = f;
+// }
+
+// function updateCelsius() {
+//   var f = document.getElementById('fahrenheit').value;
+//   var c;
+//   if ( (f == null) || (f == "") || (isNaN(f) ) )
+//   {
+//     c = "";
+//   }
+//   else
+//   {
+//     c = fahrenheitToCelsius(f);
+//   }
+//   document.getElementById('celsius').value = c;
+// }
+
+// function myround(x) {
+//   return Math.round(10 * x) / 10;
+// }
+
+// function celsiusToFahrenheit(c) {
+//   var answer = c * 9 / 5 + 32;
+//   return myround(answer);
+// }
+
+// function fahrenheitToCelsius(f) {
+//   var answer = (f - 32) * 5 / 9;
+//   return myround(answer);
+// }
